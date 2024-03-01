dd588d4a5f7b64ae9f15fbf325536e01e4039933
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-01 08:17:22 -0500

    Add main.cpp with basic command line interface
    The new main.cpp file includes a basic command line interface which starts the program, provides a 'help' option, and parses user input. This version of main.cpp uses the "Printer Utility" for option management, and has the capability to print the help message when requested.

---
+++ cli/main.cpp
@@ -0,0 +1,25 @@
+﻿//
+// Created by drew.chase on 3/1/2024.
+//
+#include <cstdio>
+
+#include "printers.h"
+#include "options_manager.h"
+
+int main(int argc, char **argv)
+{
+    printf("Hello World");
+
+    cclip::options_manager manager("Printer Utility");
+    manager.add_option("h", "help", "Prints this help message", false, false);
+    manager.add_option("o", "output", "How should the data be outputted. Ex: JSON, PLAIN_TEXT, FILE, NONE", false, false);
+
+
+    manager.parse(argc, argv);
+
+    if (manager.is_present("h"))
+    {
+        manager.print_help();
+        return 0;
+    }
+}
