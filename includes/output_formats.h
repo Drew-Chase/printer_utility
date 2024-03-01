3260a03a3028204b99b13553030d74fa08e42640
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-01 08:19:51 -0500

    Add new file for output formats enum
    A new header file 'output_formats.h' has been added to the 'includes' directory. This file includes an enumeration class 'output_format' under the 'printer_utility' namespace which gives us options for different output formats like JSON, PLAIN_TEXT, FILE and NONE.

---
+++ includes/output_formats.h
@@ -0,0 +1,16 @@
+﻿//
+// Created by drew.chase on 3/1/2024.
+//
+
+#pragma once
+
+namespace printer_utility
+{
+    enum class output_format
+    {
+        JSON,
+        PLAIN_TEXT,
+        FILE,
+        NONE
+    };
+} // printer_utility
